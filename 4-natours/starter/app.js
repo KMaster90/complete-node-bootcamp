d23d915d3e5cd92efc320feca30742425e21c39c
kmaster <capuz90@gmail.com>
2022-12-27 16:14:17 +0100

    Sezione 6: Express: Let's Start Building the Natours API!

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// app.get('/', (req, res) => {
+//   res.status(404).json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+//
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+// 2) ROUTE HANDLERS
+
+// 3) ROUTES
+app
+  .use('/api/v1/tours', tourRouter)
+  .use('/api/v1/users', userRouter);
+
+// 4) START SERVER
+module.exports = app;
